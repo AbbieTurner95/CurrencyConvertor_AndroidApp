3f822084d9d28cce012bf372f2841008f0185305
Abbie <abbieturner95@hotmail.com>
2018-02-02 15:29:28 +0000

    added ui elements to java class

--- app/src/main/java/android/example/com/ratescurrencyapp/ConversionActivity.java
+++ app/src/main/java/android/example/com/ratescurrencyapp/ConversionActivity.java
@@ -3,7 +3,6 @@ package android.example.com.ratescurrencyapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.Editable;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -18,9 +17,16 @@ public class ConversionActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_conversion);
 
-        TextView showConversion = findViewById(R.id.rates_text);
         Button convertButton = findViewById(R.id.convert_button);
         input_amount = findViewById(R.id.rate_input);
+
+        TextView full_rate_name = findViewById(R.id.fullname_rate1);
+        TextView full_rate_name2 = findViewById(R.id.fullname_rate2);
+        TextView symbol_rate_1 = findViewById(R.id.symbol_1);
+        TextView symbol_rate_2 = findViewById(R.id.symbol_2);
+        TextView abbrv_rate1 = findViewById(R.id.rate_1);
+        TextView abbrv_rate2 = findViewById(R.id.rate_2);
+        TextView output_rate = findViewById(R.id.converted_amount);
 
         Intent intent = getIntent();
         final Rate rate = intent.getExtras().getParcelable(MainActivity.RATE_KEY);  //rate to convert with
@@ -29,10 +35,7 @@ public class ConversionActivity extends AppCompatActivity {
         convertButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
                 double amount = Double.parseDouble(input_amount.getText().toString());
-
-
             }
         });
     }
